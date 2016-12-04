62827d144ff4e96bc88174b62744cbf0933967bd
Richard Fukutome <rfukutome@gmail.com>
2016-12-03 19:09:07 -0800

    Initial commit for simple scene changes

---
+++ Assets/Scripts/MenuNavigator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuNavigator : MonoBehaviour {
+
+    public void loadGameLevel(string argSceneName)
+    {
+        SceneManager.LoadScene(argSceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit was initialized");
+    }
+}
