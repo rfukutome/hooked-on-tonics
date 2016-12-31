5dad6be7b67b25715be581d7e06369b115a94812
RTA <matthew.rasz@gmail.com>
2016-12-31 00:31:40 -0800

    test pour commit

---
+++ Assets/Scripts/PourMechanic.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PourMechanic : MonoBehaviour {
+
+    public Transform Liquid;
+    [SerializeField] private float currentAmount;
+    [SerializeField] public float speed;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(currentAmount < 100)
+        {
+            currentAmount += speed * Time.deltaTime;
+        }
+        else
+        {
+            currentAmount = 100;
+        }
+        Liquid.GetComponent<Image>().fillAmount = currentAmount / 100;
+	}
+}
